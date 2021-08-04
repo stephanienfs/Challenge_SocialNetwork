3763c4769156815699387808d6e876ad14c79195
stephanienfs <stephanienicolefs@gmail.com>
2021-08-04 12:41:08 -0300

    Add handle error for worng response

--- app.js
+++ app.js
@@ -10,23 +10,39 @@ function getDelay() {
 }
 
 async function fetchData() {
+    
     const promiseTwitter = new Promise((resolve, reject) => {
         const delay = getDelay();
         setTimeout(() => {
-            const result = fetch('http://codefight.davidbanham.com/twitter').then((response) => response.json());
+            const result = fetch('http://codefight.davidbanham.com/twitter').then((response) => {
+                if (response)
+                    try {
+                        console.log("TODO SALIO BIEN");
+                        return response.json();
+                    } catch (error) {
+                        console.log(error);
+                    }
+            });
             resolve(result);
         }, delay);
-        console.log("ESTO FUE UN", resolve, reject);
     });
 
-    
+
     const promiseFacebook = new Promise((resolve, reject) => {
         const delay = getDelay;
         setTimeout(() => {
-          const result = fetch('http://codefight.davidbanham.com/facebook').then((response) => response.json());
-          resolve(result);
+            const result = fetch('http://codefight.davidbanham.com/facebook').then((response) => {
+                if (response)
+                    try {
+                        console.log("TODO SALIO BIEN");
+                        return response.json();
+                    } catch (error) {
+                        console.log(error);
+                    }
+            });
+            resolve(result);
         }, delay);
-      });
+    });
 
     /*       const promiseInstagram = new Promise((resolve, reject) => {
             setTimeout(() => {
@@ -35,11 +51,11 @@ async function fetchData() {
             }, newValue);
           }); */
 
-    Promise.all([promiseTwitter,promiseFacebook]).then(values => {
+    Promise.all([promiseTwitter, promiseFacebook]).then(values => {
         let responseObj = new Object();
         let twitterResponseObj = new Object();
         let facebookResponseObj = new Object();
-        valuesStrin = JSON.stringify(values[0]);    
+        valuesStrin = JSON.stringify(values[0]);
         responseObj.twitter = [];
 
         console.log("VALUES" + valuesStrin);
@@ -52,10 +68,10 @@ async function fetchData() {
             responseObj.twitter.push(tweet.tweet);
         });
         //responseObj.facebook = values[1].status;
-        
-        console.log("RESULT", JSON.stringify(responseObj)); // [3, 1337, "foo"]
+
+        console.log("RESULT", JSON.stringify(responseObj)); 
     });
-    
+
     /* try {
         const results = await Promise.allSettled(
             [
